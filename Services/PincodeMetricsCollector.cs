8e0ffe92b17c19bdbbe29842d9179a9ae260d947
DRAVASP SHROFF <27689043+dravasp@users.noreply.github.com>
2026-09-02 01:46:08 +0530

    Add PincodeMetricsCollector for API metrics tracking

---
+++ Services/PincodeMetricsCollector.cs
@@ -0,0 +1,30 @@
+using Prometheus;
+
+namespace VirtoCommerce.Payment.CCAvenue.Services
+{
+    public class PincodeMetricsCollector
+    {
+        private readonly Counter _apiSuccessCounter;
+        private readonly Counter _apiFailureCounter;
+        private readonly Counter _fallbackSuccessCounter;
+
+        public PincodeMetricsCollector()
+        {
+            _apiSuccessCounter = Metrics.CreateCounter(
+                "ccavenue_pincode_api_success_total",
+                "Number of successful pincode checks via courier API");
+
+            _apiFailureCounter = Metrics.CreateCounter(
+                "ccavenue_pincode_api_failure_total",
+                "Number of failed pincode checks via courier API");
+
+            _fallbackSuccessCounter = Metrics.CreateCounter(
+                "ccavenue_pincode_fallback_success_total",
+                "Number of successful pincode checks via local fallback");
+        }
+
+        public void RecordApiSuccess() => _apiSuccessCounter.Inc();
+        public void RecordApiFailure() => _apiFailureCounter.Inc();
+        public void RecordFallbackSuccess() => _fallbackSuccessCounter.Inc();
+    }
+}
