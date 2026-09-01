4293469a2c97f40f38eafa20ea857d9fb66e4041
DRAVASP SHROFF <27689043+dravasp@users.noreply.github.com>
2026-09-02 01:50:27 +0530

    Create PincodeServiceabilityTests class
    Added a new test class for Pincode serviceability tests.

---
+++ Tests/PincodeServiceabilityTests.cs
@@ -0,0 +1,16 @@
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Moq.Protected;
+using VirtoCommerce.Payment.CCAvenue.Services;
+using Xunit;
+
+namespace VirtoCommerce.Payment.CCAvenue.Tests
+{
+    public class PincodeServiceabilityTests
+    {
+        // Pincode serviceability tests as previously drafted
+    }
+}
