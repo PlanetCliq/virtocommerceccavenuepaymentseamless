1b36af842f177b5e7e59ec310ee1e38abf3ef95e
DRAVASP SHROFF <27689043+dravasp@users.noreply.github.com>
2026-09-02 01:50:08 +0530

    Add initial test class for Controllers

---
+++ Tests/ControllersTests.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using VirtoCommerce.Payment.CCAvenue.Controllers;
+using VirtoCommerce.Payment.CCAvenue.Services;
+using Xunit;
+
+namespace VirtoCommerce.Payment.CCAvenue.Tests
+{
+    public class ControllersTests
+    {
+        // Controller tests as previously drafted
+    }
+}
